4a667824851dfe21342e3442b66c0124fa2d6cab
abdullahi mohamed <abdullahi.mohamed2324@gmail.com>
2026-04-20 19:51:51 -0500

    add missing headers to generate short url api req

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -7,6 +7,9 @@ export async function generateShortUrl(req: UrlFormReq) {
 
     return fetch(url, {
         method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
         body: JSON.stringify(req),
     });
 };
