210597a6a51bce8905b22ae09726935927bfd4b0
abdullahi mohamed <abdullahi.mohamed2324@gmail.com>
2026-04-20 19:35:43 -0500

    make api call async

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -2,7 +2,7 @@ import type { UrlFormReq } from "./models";
 
 const BACKEND_URL = "http://localhost:3000"
 
-export function generateShortUrl(req: UrlFormReq) {
+export async function generateShortUrl(req: UrlFormReq) {
     let url = `${BACKEND_URL}/gen-url`;
 
     return fetch(url, {
