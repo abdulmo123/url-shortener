46683246bc64c06ccd2e1d2a249955b8f4a09643
abdullahi mohamed <abdullahi.mohamed2324@gmail.com>
2026-04-22 21:12:08 -0500

    call init_db in app startup

--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
 const cors = require('cors');
 const express = require('express');
 const app = express();
+const { db, init_db } = require('./db/db');
 
 app.use(cors({
     origin: ['http://localhost:5173']
-}))
+}));
 
 app.use(express.json());
 
 app.post('/gen-url', (req, res) => {
-  console.log(req.body.url);
-  res.json({ shortUrl: req.body.url });
+  console.log(req.body.url); 
 });
 
 app.listen(3000, () => {
     console.log('Server running ...');
+    init_db();
 });
